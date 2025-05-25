5d4e425bf9d7969ca1d4307f826a154090f490a5
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-05-25 19:46:36 -0300

    feat(mainActivity): add targeting to loginActivity using onClick on button

--- app/src/main/java/com/example/sono_de_bebe_app/MainActivity.java
+++ app/src/main/java/com/example/sono_de_bebe_app/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.sono_de_bebe_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_login);
+            }
         });
     }
 }
