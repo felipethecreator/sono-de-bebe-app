8b7d3591849536dc41f42e9ae12ac3bc3259ce10
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-05-25 22:47:54 -0300

    feat(loginActivity): implement logic to go back page

--- app/src/main/java/com/example/sono_de_bebe_app/LoginActivity.java
+++ app/src/main/java/com/example/sono_de_bebe_app/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.sono_de_bebe_app;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,15 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        ImageButton arrowBack = findViewById(R.id.arrowBack);
+
+        arrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.activity_main);
+            }
         });
     }
 }
